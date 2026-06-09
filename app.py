a584f3b6482dd0b655d4df2160c6e3925dae545f
shubham <shubhamsinghmor2312@gmail.com>
2026-06-09 23:33:34 +0530

    started with re file

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+def main():
+        st.header("Attendy - AI-Powered Attendance Management System")
+        name = st.text_input("Enter your name to mark attendance:")
+        # print(name)
+main()
