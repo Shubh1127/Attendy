3208bccb1ac683a0ac7f4bc93c4225b77f826489
shubham <shubhamsinghmor2312@gmail.com>
2026-06-10 10:46:14 +0530

    creating screens

--- app.py
+++ app.py
@@ -1,20 +1,18 @@
 import streamlit as st
+
+from src.screens.home_screen import home_screen
+from src.screens.student_screen import student_screen
+from src.screens.teacher_screen import teacher_screen
 def main():
-        st.header("Attendy - AI-Powered Attendance Management System",width="content")
-        name = st.text_input("Enter your name to mark attendance:")
-        col1,col2=st.columns(2)
-        # print(name)
-        with col1:
-             if st.button("Hi",type="primary",key="btn1",width="stretch"):
-                        print("hi",name)
-                        st.text("Hi "+name)
-        with col2:
-                if st.button("Bye",type="secondary",key="btn2",width="stretch"):
-                        print("Bye",name)
+       
+        if 'login_type' not in st.session_state:
+                 st.session_state['login_type']=None
 
-
-        st.markdown("""
-                <div style={}>Hello, I am from Attendy</div>
-
-""",unsafe_allow_html=True)
+        match st.session_state['login_type']:
+                case 'teacher':
+                        teacher_screen()
+                case 'student':
+                        student_screen()
+                case None:
+                        home_screen()
 main()
