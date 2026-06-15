62a06d2d57eed6ff7d3883dab4a99e95b9d3b82a
shubham <shubhamsinghmor2312@gmail.com>
2026-06-15 21:09:22 +0530

    creating components for teacher screen

--- src/components/footer_home.py
+++ src/components/footer_home.py
@@ -4,7 +4,7 @@ def footer_home():
 
     st.markdown(f"""
         <div style="display:flex; flex-direction:column; align-items:center; justify-content:center; margin-top:8px">
-            <p style='text-align:center; color:#E0E3FF'>Made with ❤️ by Shubh</p>
+            <p style='text-align:center; color:#E0E3FF'>Made with ❤️ by Shubham</p>
         </div>   
                 
                 """, unsafe_allow_html=True)
