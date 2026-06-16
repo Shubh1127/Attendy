965705eac9b27880127e62c2cd6e4fd0ee97c185
shubham <shubhamsinghmor2312@gmail.com>
2026-06-16 22:21:29 +0530

    done

--- src/components/footer_home.py
+++ src/components/footer_home.py
@@ -14,7 +14,7 @@ def footer_dashboard():
 
     st.markdown(f"""
         <div style="display:flex; flex-direction:column; align-items:center; justify-content:center; margin-top:8px">
-            <p style='text-align:center; color:#000000'>Made with ❤️ by Shubh</p>
+            <p style='text-align:center; color:#000000'>Made  by Shubh</p>
         </div>   
                 
                 """, unsafe_allow_html=True)
